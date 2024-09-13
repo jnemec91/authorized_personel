101c027272c443867d1c359181837977cb2e385e
Jaroslav Nemec <j.nemecc9@gmail.com>
2024-09-13 15:31:48 +0200

    edit formater to work on all cases, add tests for formater

--- Formater.py
+++ Formater.py
@@ -1,16 +1,26 @@
 import re
 
 def change_string_format(s):
-    # Define the regex pattern to match the format "A-PR-500", "S-P3-240", "C-P12-100"
-    pattern = re.compile(r"([A-Z]+-[A-Z0-9]+)-(\d+)")
-
-    # List to store the transformed strings
-    transformed_strings = []
-
+    # Define the regex pattern to match the format "A-PR-500", "S-P3-240", "C-P12-100", "D1-P1-100", "D-P6-402b", "D1-S1-403a", etc.
+    pattern = re.compile(r"\b([A-Z0-9]+-[A-Z0-9]+\d*)-(\d+[a-zA-Z]?)\b")
+    
     if isinstance(s, str):
-        s = s.strip()
-        if pattern.match(s):
-            transformed_strings.append(pattern.sub(r"\1.\2", s))
-            s = transformed_strings[0]
+        s = s.strip().upper()
+        # Replace all matching patterns in the string
+        matches = pattern.findall(s)
+        for match in matches:
+            # Construct the new format using the matched groups
+            s = f"{match[0]}.{match[1]}"
+
+        return s
 
     return s
+
+
+if __name__ == '__main__':
+    print(change_string_format('A-PR-500'))
+    print(change_string_format('S-P3-240'))
+    print(change_string_format('C-P12-100'))
+    print(change_string_format('D1-P1-100'))
+    print(change_string_format('D-P6-402b'))
+    print(change_string_format('D1-S1-402'))
