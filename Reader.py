38d3b2441e50548df0ff6cc887d05b79a518657d
Jaroslav Nemec <j.nemecc9@gmail.com>
2025-05-19 14:21:36 +0200

    Add abi_location parameter and add format_number method in Reader class

--- Reader.py
+++ Reader.py
@@ -6,13 +6,21 @@ class Reader:
                  location_blueprint: str = None,
                  location_hospital: str = None, 
                  location_name: str = None,
+                 abi_location: str = None,
                  ) -> None:
         
         self.reader_number = reader_number
         self.location_blueprint = location_blueprint
         self.location_hospital = location_hospital
         self.location_name = location_name
+        self.abi_location = abi_location
         self.authorized_personel = []
+
+    def format_number(self):
+        while len(self.reader_number) < 5:
+            self.reader_number = '0' + self.reader_number
+        
+        return self.reader_number
     
     def __str__(self):
         return f'{self.reader_number} - {self.location_name}'
@@ -21,7 +29,7 @@ class Reader:
         return f'Reader:{self.reader_number}'
 
     def __eq__(self, other):
-        return int(self.reader_number) == int(other.reader_number)
+        return self.reader_number == other.reader_number
 
     def add_person(self, person: Person) -> None:
         if person not in self.authorized_personel:
