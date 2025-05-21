9c9b9087b8916f60f77a8af67e772f33a7cf004b
Jaroslav Nemec <j.nemecc9@gmail.com>
2025-05-21 10:34:43 +0200

    Add type hints and docstrings to format_number, __str__, __repr__, and __eq__ methods in Reader class

--- Reader.py
+++ Reader.py
@@ -25,24 +25,33 @@ class Reader:
         self.abi_location = abi_location
         self.authorized_personel = []
 
-    def format_number(self):
+    def format_number(self) -> str:
+        """
+        Format the reader number to be 5 digits long, adding leading zeros if necessary.
+        """
         while len(self.reader_number) < 5:
             self.reader_number = '0' + self.reader_number
 
         return self.reader_number
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.reader_number} - {self.location_name}'
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Reader:{self.reader_number}'
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.reader_number == other.reader_number
 
     def add_person(self, person: Person) -> None:
+        """
+        Add a person to the list of authorized personnel for this reader.
+        """
         if person not in self.authorized_personel:
             self.authorized_personel.append(person)
 
     def remove_person(self, person: Person) -> None:
+        """
+        Remove a person from the list of authorized personnel for this reader.
+        """
         self.authorized_personel.remove(person)
